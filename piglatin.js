d882f2ffa7e9f94e09ba96fd52700244ca5ad1bc
adnanabdalrahman <adnan.24150@gmail.com>
2024-06-07 01:34:54 +0200

    piglatin

---
+++ piglatin.js
@@ -0,0 +1,25 @@
+const args = process.argv.slice(2);
+
+if (!args.length) {
+    console.log('please insert a text');
+    return false;
+}
+
+const translatedText = args.map(translateToPigLatin);
+console.log(translatedText.join(' '));
+
+
+
+function translateToPigLatin(word){
+    const twoConsonantStart = /^[^aeiou]{2}/;
+    const oneConsonantStart = /^[^aeiou]/;
+    const vowel  = /^[aeiou]/;
+    if(twoConsonantStart.test(word)){
+        word = word.slice(2) + word.slice(0, 2) + "ay";
+    }else if(oneConsonantStart.test(word)){
+        word = word.slice(1) + word.slice(0, 1) + "ay";
+    }else{
+        word = word + "way";
+    }
+    return word;
+}
